bbfd309e36c06c216212c707f8ec23c577a183f5
Kachkynov Alikhan <t8t228@gmail.com>
2025-03-24 21:59:01 +0600

    реализивал возможность получать последние 30 сообщений и сообщений по передаче query параметра

--- api/routers/messages.ts
+++ api/routers/messages.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import fileDb from "../fileDb";
+import { Message } from "../types";
 
 const messageRouter = express.Router();
 
@@ -8,11 +9,33 @@ messageRouter.post("/", async (req, res) => {
 
   if (!message.trim() || !author.trim()) {
     res.status(400).send({error: "Author and message must be present in the request"});
-    return
+    return;
   }
 
   const newMessage = await fileDb.addNewMessage({ author, message });
   res.send(newMessage);
+});
+
+messageRouter.get("/", async (req, res) => {
+  let messages : Message[] = await fileDb.getAllMessages();
+  const queryDate = req.query.datetime as string;
+
+  if(queryDate){
+    const date = new Date(queryDate);
+
+    if (isNaN(date.getDate())) {
+      res.status(400).send({error: "Incorrect date"});
+      return;
+    }
+
+    messages = messages.filter(message => {
+      const dateMessage = new Date(message.datetime);
+      return dateMessage > date;
+    });
+
+    const lastMessages = messages.slice(-30);
+    res.send(lastMessages);
+  }
 });
 
 export default messageRouter;
